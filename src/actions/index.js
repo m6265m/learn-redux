b0de1dd30515b84e4327c46d8d35e12d096c95fd
Maha <tausif.maha@gmail.com>
2020-12-02 11:47:53 +0500

    first commit

---
+++ src/actions/index.js
@@ -0,0 +1,13 @@
+export const increment = (number) => {
+    return {
+        type: 'INCREMENT',
+        payload: number
+    }
+}
+
+export const decrement = () => {
+    return {
+        type: 'DECREMENT'
+    }
+}
+
